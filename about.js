bb90e77315c74795ebb955b77623cf37bc925989
naturalCode4 <naturalCode4@gmail.com>
2022-04-08 15:17:11 -0600

    everything baseline running

--- about.js
+++ about.js
@@ -1,24 +1,7 @@
-console.log("hello world");
 
 
-function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	alert('form submitted')
-}
 
-const giveCompliment = event => {
-	event.preventDefault()
-
-	alert('Hey. Youre doing great. You are loved by the universe. Press OK and keep going <3')
-}
-
-// let form = document.querySelector('form#contact');
-// form.addEventListener('submit', handleSubmit);
-
-// let image = document.querySelector('img#rubberduck')
-// image.addEventListener('mouseover', giveCompliment)
-
+//NOTE: currently need to comment out home.js for this to work. not sure why
 
 const randomRestaurant = event => {
 
