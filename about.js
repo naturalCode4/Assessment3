96dd79647959d4e51599273af70c1fdbc807d814
naturalCode4 <naturalCode4@gmail.com>
2022-04-08 12:21:32 -0600

    first commit

---
+++ about.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form submitted')
+}
+
+const giveCompliment = event => {
+	event.preventDefault()
+
+	alert('Hey. Youre doing great. You are loved by the universe. Press OK and keep going <3')
+}
+
+const randomRestaurant = event => {
+}
+
+let form = document.querySelector('form#contact');
+form.addEventListener('submit', handleSubmit);
+
+let image = document.querySelector('img#rubberduck')
+image.addEventListener('mouseover', giveCompliment)
+
+let
+
+
+
---
+++ profile.js
@@ -0,0 +1,13 @@
+console.log("testing js is running in browser on html")
+
+
+const colorButton = document.getElementById('color')
+const placeButton = document.getElementById('place')
+const ritualButton = document.getElementById('ritual')
+
+colorButton.addEventListener('click', (event) => alert('Purple'))
+placeButton.addEventListener('click', (event) => alert('Quiet Rivers'))
+ritualButton.addEventListener('click', (event) => alert('Yoga'))
+
+
+
