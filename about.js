f2992df8cc436c8181ac65df5601c8c0b7a7ad08
naturalCode4 <naturalCode4@gmail.com>
2022-04-08 12:54:30 -0600

    second commit

--- about.js
+++ about.js
@@ -13,16 +13,27 @@ const giveCompliment = event => {
 	alert('Hey. Youre doing great. You are loved by the universe. Press OK and keep going <3')
 }
 
-const randomRestaurant = event => {
-}
-
 let form = document.querySelector('form#contact');
 form.addEventListener('submit', handleSubmit);
 
 let image = document.querySelector('img#rubberduck')
 image.addEventListener('mouseover', giveCompliment)
 
-let
+let restaurantSection = documement.getElementByID("random-section")
 
+const randomRestaurant = event => {
+	// event.preventDefault()
 
+	const frasierRestaurants = ['Busuba Thai', 'The Boco Restaurant', 'McDevitt Taco Supply', 'Kathmandu Restaurant II', 'Tsing Tao', "Illegal Pete's"]
+	let random = Math.floor(Math.random() * frasierRestaurants.length)
+	const restaurantName = frasierRestaurants[random]
+	console.log(restaurantName)
 
+	const generatedRestaurant = document.createElement('p')
+	restaurantSection.appendChild(generatedRestaurant)
+	generatedRestaurant.textContent=restaurantName
+
+}
+
+let restaurantButton = document.getElementByID('random-restaurant-button')
+restaurant.addEventListener('click', randomRestaurant)
